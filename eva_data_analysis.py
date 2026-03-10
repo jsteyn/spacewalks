8a58a08e664fecddb370b30e0e8439523422f13a
Jannetta Steyn <6432530+jsteyn@users.noreply.github.com>
2026-03-10 12:11:04 +0000

    Added additional crew size analysis

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import re
 
 
 def main(input_file, output_file, graph_file):
@@ -8,6 +9,9 @@ def main(input_file, output_file, graph_file):
 
     # Read the data from JSON file
     eva_data = read_json_to_dataframe(input_file)
+
+    # Calculate and add crew size to data
+    eva_data = add_crew_size_column(eva_data)
 
     # Convert and export data to CSV file
     write_dataframe_to_csv(eva_data, output_file)
@@ -30,7 +34,7 @@ def read_json_to_dataframe(input_file):
         input_file (file or str): The file object or path to the JSON file.
 
     Returns:
-         eva_df (pd.DataFrame): The cleaned and sorted data as a dataframe structure
+         eva_df (pd.DataFrame): The cleaned data as a dataframe structure
     """
     print(f'Reading JSON file {input_file}')
     # Read the data from a JSON file into a Pandas dataframe
@@ -55,6 +59,7 @@ def write_dataframe_to_csv(df, output_file):
     print(f'Saving to CSV file {output_file}')
     # Save dataframe to CSV file for later analysis
     df.to_csv(output_file, index=False, encoding='utf-8')
+
 
 def plot_cumulative_time_in_space(df, graph_file):
     """
@@ -94,7 +99,7 @@ def text_to_duration(duration):
         duration_hours (float): The duration in hours
     """
     hours, minutes = duration.split(":")
-    duration_hours = int(hours) + int(minutes)/6  # there is an intentional bug on this line (should divide by 60 not 6)
+    duration_hours = int(hours) + int(minutes)/60
     return duration_hours
 
 
@@ -106,7 +111,7 @@ def add_duration_hours(df):
         df (pd.DataFrame): The input dataframe.
 
     Returns:
-        df_copy (pd.DataFrame): A copy of df with the new duration_hours variable added
+        df_copy (pd.DataFrame): A copy of the dataframe df with the new crew_size variable added
     """
     df_copy = df.copy()
     df_copy["duration_hours"] = df_copy["duration"].apply(
@@ -114,17 +119,51 @@ def add_duration_hours(df):
     )
     return df_copy
 
+
+def calculate_crew_size(crew):
+    """
+    Calculate the size of the crew for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column containing a list of crew member names
+
+    Returns:
+        (int): The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r';', crew))-1
+
+def add_crew_size_column(df):
+    """
+    Add crew_size column to the dataset containing the value of the crew size
+
+    Args:
+        df (pd.DataFrame): The input data frame.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of the dataframe df with the new crew_size variable added
+    """
+    print('Adding crew size variable (crew_size) to dataset')
+    df_copy = df.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(
+        calculate_crew_size
+    )
+    return df_copy
+
+
 if __name__ == "__main__":
 
     if len(sys.argv) < 3:
-        input_file = './eva-data.json'
-        output_file = './eva-data.csv'
+        input_file = 'data/eva-data.json'
+        output_file = 'results/eva-data.csv'
         print(f'Using default input and output filenames')
     else:
         input_file = sys.argv[1]
         output_file = sys.argv[2]
         print('Using custom input and output filenames')
 
-    graph_file = './cumulative_eva_graph.png'
+    graph_file = 'results/cumulative_eva_graph.png'
 
     main(input_file, output_file, graph_file)
